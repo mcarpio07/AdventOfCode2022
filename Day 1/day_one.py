bac6481e1b922a42c270cf6efdc27c3e8b8ed354
mcarpio07 <miguelangelcrpclm646@gmail.com>
2022-12-02 23:54:41 +0100

    Day 1: Part 2

--- Day 1/day_one.py
+++ Day 1/day_one.py
@@ -3,13 +3,21 @@ INPUT_FILE = './Day 1/input.txt'
 
 maxCal = 0
 actualCal = 0
+cals = [] 
 with open(INPUT_FILE) as file:
     for line in file:
         if line != '\n':
             actualCal += int(line)
         else:
+            cals.append(actualCal)
             actualCal = 0
         if actualCal > maxCal:
             maxCal = actualCal
 
 print("El elfo con mas calorias cuenta con un total de " + str(maxCal) + " calorias")
+
+# Part 2
+cals.sort(reverse = True)
+threeMostCals = cals[:3]
+print(threeMostCals)
+print("La suma total de los tres elfos con mas calorias es: " + str(sum(threeMostCals)))
