cd380d309880a869dbfa7d91ad6d3a1ab610a1fe
mcarpio07 <miguelangelcrpclm646@gmail.com>
2022-12-09 02:55:39 +0100

    Day 7: Part 1

---
+++ Day 7/day_seven.py
@@ -0,0 +1,32 @@
+INPUT_FILE = './Day 7/input.txt'
+
+
+fileSizes = {}
+CD_ORDER = 'cd'
+BEFORE_DIR = '..'
+ORDER = '$'
+IS_DIR = 'dir'
+total = 0
+with open(INPUT_FILE) as file:
+    sizeDir = 0
+    for line in reversed(file.readlines()):
+        arrayLine = line.strip().split(' ')
+        if ORDER not in arrayLine and IS_DIR not in arrayLine:
+            try:
+                sizeDir += int(arrayLine[0])
+            except:
+                continue
+        elif IS_DIR in arrayLine:
+            try:
+                sizeDir += fileSizes[arrayLine[1]]
+            except:
+                continue
+        elif CD_ORDER in arrayLine and BEFORE_DIR not in arrayLine: 
+            directory = arrayLine[2]
+            fileSizes[directory] = sizeDir
+            if sizeDir <= 100000:
+                total += sizeDir
+            sizeDir = 0
+
+
+print ("total size of directorys ffdfdf <= 100000: ", total)
