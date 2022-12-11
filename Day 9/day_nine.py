ae82a5f5bd55d265b5f83db0eaff5a12bd09e731
mcarpio07 <miguelangelcrpclm646@gmail.com>
2022-12-11 03:25:53 +0100

    Day 9: Part 1 complete

--- Day 9/day_nine.py
+++ Day 9/day_nine.py
@@ -1,171 +1,102 @@
 INPUT_FILE = './Day 9/input.txt'
 
 
-HEAP = 'H'
-TAIL = 'T'
 LEFT = 'L'
 RIGHT = 'R'
 UP = 'U'
 DOWN = 'D'
-TAM_MAP = 25
 MAX_DIST = 2
-VISITED = '#'
-def chargeMap():
-    map = []
-    for i in range(TAM_MAP):
-        file = []
-        for j in range(TAM_MAP):
-            file.append(".")
-        map.append(file)
-    map[10][10] = 'H'
-    return map
+positionsT = {tuple([0,0])}
+posH = {
+    'x': 0,
+    'y': 0
+}
+posT = {
+    'x': 0,
+    'y': 0
+}
+def moveH(direction, numSteps):
+    while numSteps > 0:
+        if direction == LEFT:
+            posH['x'] = posH['x'] - 1
+            moveT()
+        if direction == RIGHT:
+            posH['x'] = posH['x'] + 1
+            moveT()
+        if direction == UP:
+            posH['y'] = posH['y'] + 1
+            moveT()
+        if direction == DOWN:
+            posH['y'] = posH['y'] - 1
+            moveT()
+        numSteps = numSteps - 1
 
-def visualizeMap(map):
-    print("Result Map:")
-    for fila in map:
-        for valor in fila:
-            print("\t", valor, end=" ")
-        print()
-
-def getH():
-    posH = {}
-    for i in range(len(map)):
-        for j in range(len(map)):
-            if map[i][j] == HEAP:
-                posH['fil'] = i
-                posH['col'] = j
-                break
-    return posH
-
-def getT():
-    posT = {}
-    for i in range(len(map)):
-        for j in range(len(map)):
-            if map[i][j] == TAIL:
-                posT['fil'] = i
-                posT['col'] = j
-                break
-    return posT
-
-def moveH(map, direction, numSteps, firstMovement):
-    posH = getH()
-    if direction == LEFT:
-        map[posH['fil']][posH['col']] = TAIL if firstMovement else '.'
-        if getT() == {} and not firstMovement:
-            map[posH['fil']][posH['col']] = TAIL
-
-        map[posH['fil']][posH['col']-numSteps] = HEAP
-
-    if direction == RIGHT:
-        map[posH['fil']][posH['col']] = TAIL if firstMovement else '.'
-        if getT() == {} and not firstMovement:
-            map[posH['fil']][posH['col']] = TAIL
-        map[posH['fil']][posH['col']+numSteps] = HEAP
-    
-    if direction == UP:
-        map[posH['fil']][posH['col']] = TAIL if firstMovement else '.'
-        if getT() == {} and not firstMovement:
-            map[posH['fil']][posH['col']] = TAIL
-        map[posH['fil']-numSteps][posH['col']] = HEAP
-    
-    if direction == DOWN:
-        map[posH['fil']][posH['col']] = TAIL if firstMovement else '.'
-        if getT() == {} and not firstMovement:
-            map[posH['fil']][posH['col']] = TAIL
-        map[posH['fil']+numSteps][posH['col']] = HEAP
-
-def checkMoveT(movement):
-    posH = getH()
-    posT = getT()
-    if (
-        abs(posT['fil'] - posH['fil']) >= 2 or
-        abs(posT['col'] - posH['col']) >= 2
-    ):
-        return True
-    return False
-
-def moveT(map):
-    posH = getH()
-    posT = getT()
-    if posT == {}: posT = posH
+def moveT():
     #DIAGONAL ↗
     if (
-        (posT['fil'] - posH['fil'] == MAX_DIST and
-        posT['col'] - posH['col'] == -1)
+        (posT['y'] - posH['y'] == -MAX_DIST and
+        posT['x'] - posH['x'] == -1)
         or
-        (posT['fil'] - posH['fil'] == 1 and
-        posT['col'] - posH['col'] == -MAX_DIST)
+        (posT['y'] - posH['y'] == -1 and
+        posT['x'] - posH['x'] == -MAX_DIST)
     ):
-        map[posT['fil']][posT['col']] = VISITED
-        map[posT['fil']-1][posT['col']+1] = TAIL
-        return 
-    
+        posT['y'] = posT['y']+1
+        posT['x'] = posT['x']+1
+        positionsT.add(tuple([posT['x'], posT['y']]))
+
     #DIAGONAL ↘
     if (
-        (posT['fil'] - posH['fil'] == -MAX_DIST and
-        posT['col'] - posH['col'] == -1)
+        (posT['y'] - posH['y'] == MAX_DIST and
+        posT['x'] - posH['x'] == -1)
         or
-        (posT['fil'] - posH['fil'] == -1 and
-        posT['col'] - posH['col'] == -MAX_DIST)
+        (posT['y'] - posH['y'] == 1 and
+        posT['x'] - posH['x'] == -MAX_DIST)
     ):
-        map[posT['fil']][posT['col']] = VISITED
-        map[posT['fil']+1][posT['col']+1] = TAIL
-        return
-    
+        posT['y'] = posT['y']-1
+        posT['x'] = posT['x']+1
+
     #DIAGONAL ↖
     if (
-        (posT['fil'] - posH['fil'] == MAX_DIST and
-        posT['col'] - posH['col'] == 1)
+        (posT['y'] - posH['y'] == -MAX_DIST and
+        posT['x'] - posH['x'] == 1)
         or
-        (posT['fil'] - posH['fil'] == 1 and
-        posT['col'] - posH['col'] == MAX_DIST)
+        (posT['y'] - posH['y'] == -1 and
+        posT['x'] - posH['x'] == MAX_DIST)
     ):
-        map[posT['fil']][posT['col']] = VISITED
-        map[posT['fil']-1][posT['col']-1] = TAIL
-        return
+        posT['y'] = posT['y']+1
+        posT['x'] = posT['x']-1
     
     #DIAGONAL ↙
     if (
-        (posT['fil'] - posH['fil'] == -MAX_DIST and
-        posT['col'] - posH['col'] == 1)
+        (posT['y'] - posH['y'] == MAX_DIST and
+        posT['x'] - posH['x'] == 1)
         or
-        (posT['fil'] - posH['fil'] == -1 and
-        posT['col'] - posH['col'] == MAX_DIST)
+        (posT['y'] - posH['y'] == 1 and
+        posT['x'] - posH['x'] == MAX_DIST)
     ):
-        map[posT['fil']][posT['col']] = VISITED
-        map[posT['fil']+1][posT['col']-1] = TAIL
-        return
-
+        posT['y'] = posT['y']-1
+        posT['x'] = posT['x']-1
+    
     #VERTICAL
-    if posT['fil'] - posH['fil'] == MAX_DIST:
-        map[posT['fil']][posT['col']] = VISITED
-        map[posT['fil']-1][posT['col']] = TAIL
-        return
-
-    if posT['fil'] - posH['fil'] == -MAX_DIST:
-        map[posT['fil']][posT['col']] = VISITED
-        map[posT['fil']+1][posT['col']] = TAIL
-        return
-
+    if posT['y'] - posH['y'] == -MAX_DIST:
+        posT['y'] = posT['y']+1
+    
+    if posT['y'] - posH['y'] == MAX_DIST:
+        posT['y'] = posT['y']-1
+    
     #HORIZONTAL
-    if posT['col'] - posH['col'] == MAX_DIST:
-        map[posT['fil']][posT['col']] = VISITED
-        map[posT['fil']][posT['col']-1] = TAIL
-        return
-
-    if posT['col'] - posH['col'] == -MAX_DIST:
-        map[posT['fil']][posT['col']] = VISITED
-        map[posT['fil']][posT['col']+1] = TAIL
-        return
-
+    if posT['x'] - posH['x'] == -MAX_DIST:
+        posT['x'] = posT['x']+1
+    
+    if posT['x'] - posH['x'] == MAX_DIST:
+        posT['x'] = posT['x']-1
+    
+    positionsT.add(tuple([posT['x'], posT['y']]))
 
 with open(INPUT_FILE) as file:
-    map = chargeMap()
-    visualizeMap(map)
-    firstMovement = True
     for line in file.readlines():
         movement = line.strip().split()
-        moveH(map, movement[0], int(movement[1]), firstMovement)
-        firstMovement = False
-        moveT(map)
-        visualizeMap(map)
+        moveH(movement[0], int(movement[1]))
+
+
+print("Total visits: ", len(positionsT))
